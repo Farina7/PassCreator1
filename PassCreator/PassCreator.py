a66cb9682b9117aae52b9457b631e5f07aa4881b
Davide Elia Di resta <davide.diresta@outlook.it>
2021-12-20 22:17:17 +0000

    Creation of program working, Still buggy tho

--- PassCreator/PassCreator.py
+++ PassCreator/PassCreator.py
@@ -1,1 +1,18 @@
-import pyodbc
+import pyodbc 
+import random as r
+
+server = 'LAPTOP-2J9AEQ6E'
+database = 'PasswordCreatorDB'
+
+ix = r.randint(1, 60453)
+idn = str(ix)
+connection = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                      "Server=" + server + ";"
+                      "Database=" + database + ";"
+                      "Trusted_Connection=yes;")
+conn = pyodbc.connect = (connection)
+
+cursor = conn.cursor()
+cursor.execute("exec sp_choose_words " + idn )
+for i in cursor:
+    print(i)
