6fa34d203d3759d97f7794985b2faa4438ed53a9
Davide Elia Di resta <davide.diresta@outlook.it>
2021-12-24 00:10:35 +0000

    Now the program connect to db, returns the data and transform it in a normal string withouit characters. There are still problem with the random Id generator and the loop to call the words only loops 2 times. Porcodio

--- PassCreator/PassCreator.py
+++ PassCreator/PassCreator.py
@@ -1,18 +1,50 @@
 import pyodbc 
-import random as r
+import numpy as np
+from datetime import datetime
+import sys
+
+
 
 server = 'LAPTOP-2J9AEQ6E'
 database = 'PasswordCreatorDB'
 
-ix = r.randint(1, 60453)
-idn = str(ix)
-connection = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=" + server + ";"
-                      "Database=" + database + ";"
-                      "Trusted_Connection=yes;")
-conn = pyodbc.connect = (connection)
+try:
 
-cursor = conn.cursor()
-cursor.execute("exec sp_choose_words " + idn )
-for i in cursor:
-    print(i)
+    connection = pyodbc.connect("Driver={SQL Server Native Client 11.0};" #connect to db
+                              "Server=" + server + ";"
+                              "Database=" + database + ";"
+                              "Trusted_Connection=yes;")
+except: 
+    print("There is no connection to the database or a problem with the connection string")
+    sys.exit(1) #remeber to add the exit from the program, so it will not continue and create unhandled problems
+else:
+
+    try:
+        conn = pyodbc.connect = (connection)
+        cursor = conn.cursor() 
+        
+    except:
+        print("There was a problem connecting to the database ")
+        sys.exit(1)
+    else:
+        for n in range(4):
+            idn = str(np.random.randint(1, 60453))
+            try:
+                cursor.execute("exec sp_choose_words " + idn ) #execute the stored procedure
+            except:
+                print("There was a problem with retriving the data")
+                sys.exit(1)
+            else:
+  
+
+                for row in cursor: #transform the cursor in a normal string and delete the characters not needed
+                    word = str(row) 
+                    sc  ="(',) [\]^_`{|}~'()*+, -./:;<>"
+                    for s in sc:
+                        word = word.replace(s, "").capitalize() 
+                        print (idn)
+                words = word + word + str(np.random.randint(1,9))
+    
+
+            print(words)
+
